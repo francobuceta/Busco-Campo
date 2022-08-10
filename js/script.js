b1c90fe7f7797c36d2d778a51ff0883e13b59af8
Franco Buceta <francobuceta95@gmail.com>
2022-08-10 20:39:01 -0300

    Cambios agregados, responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-document.querySelector(".burguer").addEventListener("click", );
+function hamburgerMenu(panelBtn, panel, menuLink) {
+    const d = document;
+    d.addEventListener("click", e=> {
+        if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
+            d.querySelector(panel).classList.toggle("panel-active");
+        }
+
+        if (e.target.matches(menuLink)) {
+            d.querySelector(panel).classList.remove("panel-active");
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", e=> {
+    hamburgerMenu(".panel-btn", ".panel", ".menu a");
+})
