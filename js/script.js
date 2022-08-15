ea163ddcc6946f3ba7742fb232eaad64252f80cd
Franco Buceta <francobuceta95@gmail.com>
2022-08-15 20:19:42 -0300

    Barra buscadora funcional

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Menu hamburguesa
 function hamburgerMenu(panelBtn, panel, menuLink) {
     const d = document;
     d.addEventListener("click", e=> {
@@ -14,3 +15,34 @@ function hamburgerMenu(panelBtn, panel, menuLink) {
 document.addEventListener("DOMContentLoaded", e=> {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
 })
+
+
+//Bucador
+
+let barSearch = document.getElementById("search");
+let inputSearch = document.getElementById("inputSearch");
+let boxSearch = document.getElementById("cuadro-de-busquedas");
+
+function buscadorInterno() {
+    let filtro = inputSearch.value.toUpperCase();
+    let lista = boxSearch.getElementsByTagName("li");
+
+    for (let i = 0; i < lista.length; i++) {
+        let a = lista[i].getElementsByTagName("a")[0];
+        let textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filtro) > -1) {
+            lista[i].style.display = "";
+            boxSearch.style.display = "block";
+
+            if (inputSearch.value === "") {
+                boxSearch.style.display = "none";
+            }
+
+        } else {
+            lista[i].style.display = "none";
+        }
+    }
+}
+
+document.getElementById("inputSearch").addEventListener("keyup", buscadorInterno);
