2b9ae6aa4e2b5b65d81ec1df35f4543a2ba2741b
Franco Buceta <francobuceta95@gmail.com>
2022-08-16 19:09:24 -0300

    Sitio sin barra de busqueda

--- js/script.js
+++ js/script.js
@@ -17,32 +17,3 @@ document.addEventListener("DOMContentLoaded", e=> {
 })
 
 
-//Bucador
-
-let barSearch = document.getElementById("search");
-let inputSearch = document.getElementById("inputSearch");
-let boxSearch = document.getElementById("cuadro-de-busquedas");
-
-function buscadorInterno() {
-    let filtro = inputSearch.value.toUpperCase();
-    let lista = boxSearch.getElementsByTagName("li");
-
-    for (let i = 0; i < lista.length; i++) {
-        let a = lista[i].getElementsByTagName("a")[0];
-        let textValue = a.textContent || a.innerText;
-
-        if(textValue.toUpperCase().indexOf(filtro) > -1) {
-            lista[i].style.display = "";
-            boxSearch.style.display = "block";
-
-            if (inputSearch.value === "") {
-                boxSearch.style.display = "none";
-            }
-
-        } else {
-            lista[i].style.display = "none";
-        }
-    }
-}
-
-document.getElementById("inputSearch").addEventListener("keyup", buscadorInterno);
